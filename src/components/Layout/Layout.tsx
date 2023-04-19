e983aa068a73c15bbf934b34b1669b406ea81bcd
Turoczek <bartosztrzonek@interia.pl>
2023-04-19 20:59:37 +0200

    Portfolio initial commit

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import React, { FC, useContext } from "react";
 import { Header, PageRow, ScrollToTop } from "@/components";
 import { GlobalStateContext } from "@/providers/GlobalStateProvider";
 import { useActor } from "@xstate/react";
+import Head from "next/head";
 import { LayoutProps } from "./Layout.types";
 
 export const Layout: FC<LayoutProps> = ({ children }) => {
@@ -13,6 +14,13 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
 
   return (
     <>
+      <Head>
+        <title>Turok porfolio</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="logo.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="logo.png" />
+
+        <meta name="robots" content="noindex" />
+      </Head>
       <PageRow>
         <Header />
       </PageRow>
